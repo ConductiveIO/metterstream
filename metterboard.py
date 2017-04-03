19f9ab7336b427cd57f3c3ab4a215015f200023e
conductiveio <grodin.robby@gmail.com>
2017-04-02 22:15:50 -0400

    pep 8ed

--- metterboard.py
+++ metterboard.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, emit
 import os
 from multiprocessing import Process
 
-
+# Configure global variables
 CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
@@ -16,26 +16,43 @@ app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'metterboard.db')
 ))
 
-socketio = SocketIO(app,  async_mode='gevent', ping_timeout=30, logger=False, engineio_logger=False)
+socketio = SocketIO(
+    app,
+    async_mode='gevent',
+    ping_timeout=30,
+    logger=False,
+    engineio_logger=False
+)
 
-# Connections to Twitter
+# Connections to Twitter Streams.
+# Currently a singleton, but may expand in next iteration.
 streams = []
 
+
 def connect_db():
+    """
+    Connect to a local db file containing the schema for our twitter board.
+    The check_same_thread param allows for threads to share a db connection.
+    """
     db = sqlite3.connect(app.config['DATABASE'], check_same_thread=False)
     db.row_factory = sqlite3.Row
     return db
 
+
 def get_db():
-    """Opens a new database connection if there is none yet for the
+    """
+    Opens a new database connection if there is none yet for the
     current application context.
     """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 def init_db():
-    """Initializes the database."""
+    """
+    Initializes the database schema if no db file exists yet.
+    """
     with app.app_context():
         db = get_db()
         with app.open_resource('mb_schema.sql', mode='r') as f:
@@ -43,122 +60,198 @@ def init_db():
         db.commit()
         return db
 
-# Initialize DB
+
+# Initialize the db schema on startup
 init_db()
+
 
 @socketio.on('connection')
 def handle_connection(msg):
+    """
+    Flush the db contents onto the client upon connection.
+    """
     db = get_db()
-    cur = db.execute('select id, text, user, screen_name, profile_image_url, media_url from tblTweet order by id desc')
+    cur = db.execute("""
+        SELECT id, text, user, screen_name, profile_image_url, media_url
+        FROM tblTweet ORDER BY id DESC;
+        """)
     tweets = cur.fetchall()
     for tweet in tweets:
         json_data = jsonify({
             'id': tweet['id'],
-            'text': tweet['text'], 
-            'user': tweet['user'], 
-            'screen_name': tweet['screen_name'], 
+            'text': tweet['text'],
+            'user': tweet['user'],
+            'screen_name': tweet['screen_name'],
             'profile_image_url': tweet['profile_image_url'],
             'media_url': tweet['media_url']
         })
         socketio.emit(msg['client'], json_data.data)
 
+
 @socketio.on('delete_tweet')
 def delete_tweet(msg):
+    """
+    Remove a flagged tweet from the database to prevent being displayed.
+    TODO: should this be a flag rather than a deletion?
+    """
     tweet_id = msg.get('id')
     if tweet_id:
         delete_from_db(tweet_id)
         socketio.emit('delete_tweet_from_display', {'id': tweet_id})
 
+
 def delete_from_db(tweet_id):
+    """
+    db action to remove tweet by id
+    """
     db = get_db()
     cur = db.execute("""
         DELETE FROM tblTweet WHERE ID = %s;
-    """ % (tweet_id))
+        """ % (tweet_id))
     db.commit()
+
 
 @app.route('/')
 def show_entries():
+    """
+    Display endpoint. Delivers an html scaffold unto which
+    we will project 140 characters of pure genius.
+    """
     return render_template('show_tweets.html')
+
 
 @app.route('/admin')
 def admin():
+    """
+    Render the admin panel
+    """
     return render_template('admin.html')
+
 
 @app.route('/track/<hashtag>', methods=['GET'])
 def track(hashtag):
+    """
+    Asynchronous endpoint used to initiate a twitter stream.
+    Stream executes in a background process.
+    """
     halt_streams()
     stream = track(hashtag)
     streams.append(stream)
     return make_response('', 200)
 
+
 @app.route('/clear_db', methods=['GET'])
 def clear_db():
+    """
+    Remove all the data in the db file.
+    """
     db = get_db()
-    db.execute('DELETE FROM tblTweet;')
+    db.execute("""DELETE FROM tblTweet;""")
     db.commit()
-    return make_response()
+    return make_response('', 200)
+
 
 @app.route('/halt', methods=['GET'])
 def halt():
+    """
+    Stop the firehose.
+    """
     halt_streams()
     return make_response('', 200)
 
+
 def halt_streams():
+    """
+    Kill all streams in the registry.
+    """
     for stream in streams:
         stream.disconnect()
 
+
+def track(hashtag):
+    """
+    Authenticate to Twitter API and create an asycnhronous stream of tweets
+    """
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = API(auth)
+
+    listener = TweetListener(get_db())
+    listener.api = api
+    stream = Stream(api.auth, listener)
+    stream.filter(track=[hashtag], async=True)
+
+    return stream
+
+
 class TweetListener(StreamListener):
+    """
+    Custom stream listener.
+    Streams tweets in a background thread and makes them available
+    to clients and persistent data store.
+    """
 
     def __init__(self, db):
         self.db = db
 
     def on_status(self, data):
-        # Persist tweet to DB
+        """
+        Handle an incoming tweet on the stream.
+        Send it to the db, and to the clients.
+        Because this is happening in another process,
+        need to fake an app context.
+        """
         with app.test_request_context():
-            media_url = data.entities['media'][0]['media_url'] if data.entities.get('media') else None
-            self.db.execute(
-                    """insert into tbltweet (id, text, user, screen_name, profile_image_url, media_url) 
-                    values (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");
-                    """ % (data.id_str.encode('utf-8'),
-                        data.text.replace('"', '\'').encode('utf-8').strip(), 
-                        data.user.name.replace('"', '\'').encode('utf-8').strip(), 
-                        data.author.screen_name.replace('"', '\'').encode('utf-8').strip(), 
-                        data.author.profile_image_url.replace('"', '\'').encode('utf-8').strip(),
-                        media_url.replace('"', '\'').encode('utf-8').strip() if media_url else str(media_url)
-                    ))
-            self.db.commit()
-            
-            # Stream tweet to client
-            data = {
-                'id': data.id_str.encode('utf-8'),
-                'text': data.text.encode('utf-8').strip(), 
-                'user': data.user.name.encode('utf-8').strip(), 
-                'screen_name': data.author.screen_name.encode('utf-8').strip(), 
-                'profile_image_url': data.user.profile_image_url.encode('utf-8').strip()
-            }
-
-            # Add media url if present
-            if media_url:
-                data['media_url'] = media_url.replace('"', '\'').encode('utf-8').strip()
-            
-            # Transmit tweet to clients
-            json_data = jsonify(data)
-            socketio.emit('tweetstream', json_data.data.encode('utf-8').strip())
-            socketio.emit('admin_tweetstream', json_data.data.encode('utf-8').strip())
+            self.persist_tweet(data)
+            self.transmit_tweet(data)
         return True
 
-def track(hashtag):
-    print 'setting up tracking'
-    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-    print 'auth'
-    if CONSUMER_KEY:
-        print CONSUMER_KEY
-    api = API(auth)
-    
-    listener = TweetListener(get_db())
-    listener.api = api
-    stream = Stream(api.auth, listener)
-    stream.filter(track=[hashtag], async=True)
-   
-    return stream
+    def persist_tweet(self, data):
+        """
+        Persist the vital tweet data to a local sqlite db file
+        """
+        media_url = data.entities['media'][0]['media_url'] if data.entities.get(
+            'media') else None
+        self.db.execute("""
+                INSERT INTO TBLTWEET (
+                    id, text, user, screen_name, profile_image_url, media_url
+                ) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");
+                """ % (data.id_str.encode('utf-8'),
+                       data.text.replace('"', '\'').encode('utf-8').strip(),
+                       data.user.name.replace(
+                           '"', '\'').encode('utf-8').strip(),
+                       data.author.screen_name.replace(
+                           '"', '\'').encode('utf-8').strip(),
+                       data.author.profile_image_url.replace(
+                           '"', '\'').encode('utf-8').strip(),
+                       media_url.replace('"', '\'').encode(
+                           'utf-8').strip() if media_url else str(media_url)
+                       )
+        )
+        self.db.commit()
+
+    def transmit_tweet(self, data):
+        """
+        Send the tweet to all clients
+        """
+        # Stream tweet to client
+        tweet = {
+            'id': data.id_str.encode('utf-8'),
+            'text': data.text.encode('utf-8').strip(),
+            'user': data.user.name.encode('utf-8').strip(),
+            'screen_name': data.author.screen_name.encode('utf-8').strip(),
+            'profile_image_url': data.user.profile_image_url.encode('utf-8').strip()
+        }
+
+        # Add media url if present
+        media_url = data.entities['media'][0]['media_url'] if data.entities.get(
+            'media') else None
+        if media_url:
+            tweet['media_url'] = media_url.replace(
+                '"', '\'').encode('utf-8').strip()
+
+        # Transmit tweet to clients
+        json_data = jsonify(tweet)
+        socketio.emit('tweetstream', json_data.data.encode('utf-8').strip())
+        socketio.emit('admin_tweetstream',
+                      json_data.data.encode('utf-8').strip())
